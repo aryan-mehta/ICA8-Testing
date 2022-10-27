1019757a8cc5dbf94193f2ba64bfad6ed4f084d1
aryan-mehta <aryancountry@gmail.com>
2022-10-27 15:35:22 -0700

    initial commit of skeleton code

---
+++ ICA8_Testing/src/urinals.java
@@ -0,0 +1,28 @@
+//Author - Aryan Mehta <1225456817>
+import java.util.Scanner;
+import org.junit.jupiter.api.Test;
+
+public class urinals {
+    public static int number_of_free_urinals(String urinals) {
+        // Returns the number of free available urinals as per the rule.
+        return 0;
+    }
+    public static String get_input_string(){
+        // Function to take the input from the keyboard.
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your input: ");
+        String urinals=sc.next();
+        return urinals;
+    }
+    public static void open_file(){
+        // Function to read te input file
+    }
+    public Boolean goodString(String urinals){
+        // checks to see if valid string is given as input
+        System.out.println ("Not yet implemented");
+        return true;
+    }
+    public static void main(String[] args){
+
+    }
+}
