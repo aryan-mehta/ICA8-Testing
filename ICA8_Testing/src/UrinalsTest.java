109e5343c6340cddc2d24b5e8807d421501c46f1
aryan-mehta <aryancountry@gmail.com>
2022-10-27 18:14:59 -0700

    added main logic for keyboard input

--- ICA8_Testing/src/UrinalsTest.java
+++ ICA8_Testing/src/UrinalsTest.java
@@ -2,13 +2,13 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class urinalsTest {
-    private urinals urinal_object= new urinals();
+public class UrinalsTest {
+//    private urinals urinal_object= new urinals();
 
     @Test
     void Testcase1() {
         System.out.println("====== Aryan Mehta == TEST ONE EXECUTED =======");
-        assertEquals(-1,urinal_object.number_of_free_urinals("1101"));
+        assertEquals(-1, urinals.number_of_free_urinals("1101"));
     }
 
 }
--- ICA8_Testing/src/urinals.java
+++ ICA8_Testing/src/urinals.java
@@ -1,43 +1,70 @@
 //Author - Aryan Mehta <1225456817>
 import java.util.Scanner;
-import org.junit.jupiter.api.Test;
 
 public class urinals {
     public static int number_of_free_urinals(String urinals) {
         // Returns the number of free available urinals as per the rule.
         //Base Cases
-        if (good_or_bad_string(urinals)==false) return -1;
+        if (!good_or_bad_string(urinals)) return -1;
         if (urinals.equals("0")) return 1;
         if (urinals.equals("1")) return 0;
 
-        return 0;
+        char[] urinals_array = urinals.toCharArray();
+        int counter = 0;
+
+        for (int i = 0; i < urinals_array.length - 1; i++) {
+            if (i == 0) {
+                if (urinals_array[i] == '0' && urinals_array[i + 1] == '0') {
+                    urinals_array[i] = '1';
+                    counter++;
+                }
+            }
+            if (i > 0) {
+                if (urinals_array[i] == '0' && urinals_array[i - 1] == '0' && urinals_array[i + 1] == '0') {
+                    urinals_array[i] = '1';
+                    counter++;
+                }
+            }
+        }
+        if (urinals_array[urinals_array.length - 1] == '0' && urinals_array[urinals_array.length - 2] == '0') {
+            counter++;
+        }
+        return counter;
+
     }
-    public static String get_input_string(){
+    public static void get_keyboard_input_string(){
         // Function to take the input from the keyboard.
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter your input: ");
-        String input_urinals=sc.next();
-        return input_urinals;
+
+        while(true){
+            System.out.print("Hint: Enter -1 to exit\nEnter your input: ");
+            String input_string=sc.next();
+            if(input_string.equals("-1")){
+                break;
+            }
+            int ans=number_of_free_urinals(input_string);
+            System.out.print("Number of people that can use the urinals: ");
+            System.out.println(ans);
+        }
     }
     public static void open_file(){
         // Function to read te input file
     }
     public static Boolean good_or_bad_string(String input_string){
         // checks to see if valid string is given as input
+        if (input_string.length()<1) return false;
         if (input_string.length()>20) return false;
         if (input_string.contains("11")) return false;
         char[] urinals_array = input_string.toCharArray();
         for (int i = 0; i < urinals_array.length - 1; i++){
-            if(urinals_array[i]!='0' || urinals_array[i]!='1'){
+            if(urinals_array[i]!='0' && urinals_array[i]!='1'){
                 return false;
             }
         }
         return true;
     }
     public static void main(String[] args){
-        String input_urinal_string=get_input_string();
-        int ans=number_of_free_urinals(input_urinal_string);
-        System.out.print("Number of people that can use the urinals: ");
-        System.out.println(ans);
+        get_keyboard_input_string();
+
     }
 }
